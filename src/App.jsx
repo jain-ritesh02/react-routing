7bc3bd53b5d8fcb0785e556315d7ce27a9bf7085
Jainritgithub <riteshjn022@gmail.com>
2024-04-23 17:58:08 +0530

    react-routing

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Home from './Components/Home'
+import Navbar from './Components/Navbar'
+import About from './Components/About'
+import Create from './Components/Create'
+import { Route, Routes } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <div>
+
+    <Navbar/>
+
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/create" element={<Create />} />``
+    </Routes>
+
+    </div>
+  )
+}
+
+export default App
